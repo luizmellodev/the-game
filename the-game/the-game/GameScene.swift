320fe7e299bbb7c24f81cc4359e2937a3252f399
Luiz Eduardo <luizdudumr@gmail.com>
2021-09-09 17:29:14 -0300

    base coded addded :fire:

--- the-game/the-game/GameScene.swift
+++ the-game/the-game/GameScene.swift
@@ -10,8 +10,56 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    // MARK: - Nodes
+    
+    private var circle: SKShapeNode = SKShapeNode()
+    private var bar: SKShapeNode = SKShapeNode()
+    private var squares: SKShapeNode = SKShapeNode()
+    private var triangles: SKShapeNode = SKShapeNode()
+    
+    
+    
+    // MARK: - Init
+    
     override func didMove(to view: SKView) {
-        backgroundColor = SKColor.red
-        NSLog("We have loaded the start screen")
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        createCircle()
+        createBar()
+        //backgroundColor = SKColor.red
     }
+    
+    // MARK: - Functions
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+            let location = touch.location(in: self)
+            
+            bar.position.x = location.x
+            bar.position.y = location.y
+        }
+    }
+    
+    func createCircle() {
+        let circle = SKShapeNode(circleOfRadius: 20)
+        let circlePosition = CGPoint(x: 0.0, y: -320.0)
+        
+        circle.position = circlePosition
+        circle.fillColor = UIColor.white
+        
+        addChild(circle)
+    }
+    
+    func createBar() {
+        let bar = SKShapeNode(rectOf: CGSize(width: 130, height: 3))
+        let barPosition = CGPoint(x: 0.0, y: -260.0)
+        
+        bar.position = barPosition
+        bar.fillColor = UIColor.white
+        
+        addChild(bar)
+    }
+    
+    //MARK: - Update
+    
+    
 }
--- the-game/the-game/GameViewController.swift
+++ the-game/the-game/GameViewController.swift
@@ -28,7 +28,7 @@ class GameViewController: UIViewController {
             
             view.ignoresSiblingOrder = true
             
-            view.showsFPS = true
+            view.showsFPS = true    
             view.showsNodeCount = true
         }
     }
