abe01a34f58d09257c309aa4b3bc4d0931a66057
Luiz Eduardo <luizdudumr@gmail.com>
2021-09-10 19:12:29 -0300

    text menu changed

--- the-game/the-game/StartGameScene.swift
+++ the-game/the-game/StartGameScene.swift
@@ -10,9 +10,13 @@ import SpriteKit
 class StartGameScene: SKScene {
     
     override func didMove(to: SKView) {
-        let startGameButton = SKSpriteNode(imageNamed: "botao")
-        startGameButton.position = CGPoint(x: size.width/2, y: size.height/2 - 100)
+        self.backgroundColor = UIColor.white
+        let startGameButton = SKLabelNode(fontNamed: "SF Pro Display")
+        startGameButton.position = CGPoint(x: size.width/2, y: size.height/2)
         startGameButton.name = "startgame"
+        startGameButton.text = "Tap to play!"
+        startGameButton.fontSize = 20
+        startGameButton.fontColor = SKColor.black
         addChild(startGameButton)
     }
     
@@ -20,13 +24,11 @@ class StartGameScene: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
-            let touchedNode = atPoint(location)
-            if touchedNode.name == "startgame" {
-                let gameOverScene = GameScene(size: size)
-                gameOverScene.scaleMode = scaleMode
-                let transitionType = SKTransition.flipVertical(withDuration: 2.0)
-                view?.presentScene(gameOverScene,transition: transitionType)
-            }
+            let gameOverScene = GameScene(size: size)
+            gameOverScene.scaleMode = scaleMode
+            let transitionType = SKTransition.flipVertical(withDuration: 2.0)
+            view?.presentScene(gameOverScene,transition: transitionType)
+            
         }
     }
     
