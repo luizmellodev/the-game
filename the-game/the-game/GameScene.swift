9b69b615e0b9a34fd27409dc2186ed6a154b25ac
Luiz Eduardo <luizdudumr@gmail.com>
2021-09-15 14:54:20 -0300

    level difficulty adjusted and some design fixed =)

--- the-game/the-game/GameScene.swift
+++ the-game/the-game/GameScene.swift
@@ -16,13 +16,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     // MARK: - Nodes
     
     private var circle: SKShapeNode = SKShapeNode()
-    private var bar: SKSpriteNode = SKSpriteNode()
+    private var bar: SKShapeNode = SKShapeNode()
     private var box: SKShapeNode = SKShapeNode()
     private var levelText: SKLabelNode = SKLabelNode()
     private var godmodText: SKLabelNode = SKLabelNode()
     private var godmod: Bool = false
     private var barWidth: Double = 130
     private var isTouched: Bool = false
+    private var scoreLabel2: SKLabelNode = SKLabelNode()
     var circleGravity: Bool = false
     var speedBox: Double = 2
     var speedCircle: Double = 1.5
@@ -50,23 +51,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         switch level {
         case 10:
             speedCircle = 1
-            speedBox = 2
+            speedBox = 1.5
             circleAction(speedCircle: speedCircle)
         case 30:
-            speedBox = 1.9
+            speedBox = 1.3
             speedCircle = 0.9
             circleAction(speedCircle: speedCircle)
         case 50:
-            speedBox = 1.7
+            speedBox = 1.0
             speedCircle = 0.8
         case 100:
-            speedBox = 1.4
+            speedBox = 0.7
             speedCircle = 0.4
         case 130:
-            speedBox = 1.5
             circleGravity = true
         default:
-            speedBox = 2
+            speedBox = 0.5
             circleGravity = false
         }
     }
@@ -79,25 +79,35 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         levelText.fontColor = SKColor.gray
         levelText.text = "level \(count)"
         levelText.zPosition = -1
+        levelText.alpha = 0
         
-        let wait = SKAction.wait(forDuration: 1)
         let fadeIn = SKAction.fadeIn(withDuration: 2)
         let fadeOut = SKAction.fadeOut(withDuration: 2)
-        let sequence = SKAction.sequence([wait, fadeIn, fadeOut])
+        
+        let sequence = SKAction.sequence([fadeIn, fadeOut])
         levelText.run(sequence)
         self.levelText = levelText
         
         self.addChild(levelText)
+        _ = Timer.scheduledTimer(timeInterval: speedBox, target: self, selector: #selector(createBox), userInfo: nil, repeats: true)
     }
     
     func createScore(){
         scoreLabel = SKLabelNode()
         scoreLabel.name = "scoreLabel"
-        scoreLabel.position = CGPoint(x: ((self.view?.bounds.size.width ?? 0) / 2) - 30, y: ((self.view?.bounds.size.height ?? 0) / 3) + 20)
+        scoreLabel.position = CGPoint(x: ((self.view?.bounds.size.width ?? 0) / 2) - 30, y: ((self.view?.bounds.size.height ?? 0) / 3) + 10)
         scoreLabel.fontSize = 30
         scoreLabel.fontColor = SKColor.label
         scoreLabel.text = "\(count)"
         
+        scoreLabel2 = SKLabelNode()
+        scoreLabel2.name = "scoreLabel2"
+        scoreLabel2.fontSize = 20
+        scoreLabel2.fontColor = SKColor.label
+        scoreLabel2.text = "Score"
+        scoreLabel2.position = CGPoint(x: ((self.view?.bounds.size.width ?? 0) / 2) - 40, y: ((self.view?.bounds.size.height ?? 0) / 3) + 35)
+        
+        self.addChild(scoreLabel2)
         self.addChild(scoreLabel)
     }
     
@@ -193,6 +203,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let circlePosition = CGPoint(x: 0.0, y: -320.0)
         circle.position = circlePosition
         circle.fillColor = UIColor.label
+        circle.strokeColor = UIColor.label
         circle.name = "circle"
         
         self.addChild(circle)
@@ -207,9 +218,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func createBar() {
-        bar = SKSpriteNode(color: UIColor.label, size:  CGSize(width: self.barWidth, height: 10))
+        //        bar = SKSpriteNode(color: UIColor.label, size:  CGSize(width: self.barWidth, height: 10))
+        bar = SKShapeNode(rectOf: CGSize(width: self.barWidth, height: 10), cornerRadius: 7.0)
+        bar.fillColor = UIColor.label
         bar.position = CGPoint(x: 0.0, y: -260.0)
-        bar.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        bar.strokeColor = UIColor.label
+        //        bar.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         
         self.addChild(bar)
     }
@@ -220,10 +234,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             self.backgroundColor = UIColor.darkGray
             self.godmodText.alpha = 1.0
             bar.run(SKAction.moveTo(x: circle.position.x, duration: 0.0))
-            self.bar.scale(to: CGSize(width: self.barWidth, height: 10))
         }
         else{
-            self.bar.scale(to: CGSize(width: self.barWidth, height: 10))
             self.backgroundColor = UIColor.systemBackground
             self.godmodText.alpha = 0.0
             //            childNode(withName: "godmodText")?.removeFromParent()
@@ -238,8 +250,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         //posicao random das caixas
         let randomNumber = arc4random_uniform(2)
         let x: CGFloat = randomNumber == 0 ? 1 : -1
-        box.position = CGPoint(x: (CGFloat(arc4random_uniform(UInt32(UIScreen.main.bounds.width))) * x), y: self.frame.size.height/2)
+        box.position = CGPoint(x: (CGFloat(arc4random_uniform(UInt32(UIScreen.main.bounds.width - 30))) * x), y: self.frame.size.height/2)
         box.fillColor = UIColor.label
+        box.strokeColor = UIColor.label
         box.name = "box"
         
         //coloquei boxPhysicsBody aqui pra n precisar por a funcao setPhysics no update
@@ -331,12 +344,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             lastUpdateTime = currentTime
             count += 1
             scoreLabel.text = "\(count)"
-            if(count == 10 || count == 20 || count == 30 || count == 50){
+            if(count == 5 || count == 20 || count == 30 || count == 50){
                 levelManagement(level: count)
                 levelText.removeFromParent()
                 showLevel()
             }
-            _ = Timer.scheduledTimer(timeInterval: speedBox, target: self, selector: #selector(createBox), userInfo: nil, repeats: true)
             print("Círculo: \(speedCircle)")
             print("Box: \(speedBox)")
         }
